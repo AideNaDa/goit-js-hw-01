70fd14d4d60321d193423d9a6ca685fd1446d8a9
AideNaDa <152689555+AideNaDa@users.noreply.github.com>
2024-02-23 20:08:46 +0100

    f

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-var quantity
-var pricePedDroid
+var quantity;
+var pricePedDroid;
 function makeTransaction(quantity, pricePedDroid) {
     return `You ordered ${quantity} droids worth ${quantity * pricePedDroid} credits!`;
-}
+};
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
-var content
-var padding
-var border
+var content;
+var padding;
+var border;
 function getElementWidth(content, padding, border) {
-    retern(Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseInt(border) * 2);
-}
+    retern (Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseInt(border) * 2);
+};
 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
